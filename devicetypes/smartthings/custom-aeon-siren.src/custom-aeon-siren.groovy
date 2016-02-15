d77a099ecd743ac7a8fde78942d3ca912e0b0021
Eric Scherrer <escherrer@gmail.com>
2016-02-15 08:36:15 -0500

    ...

--- devicetypes/smartthings/custom-aeon-siren.src/custom-aeon-siren.groovy
+++ devicetypes/smartthings/custom-aeon-siren.src/custom-aeon-siren.groovy
@@ -152,8 +152,8 @@ def test() {
 def chime(Short duration) {    
     log.info "Chime with duration of ${duration}"
     
-    if (duration == null || duration < 100) {
-        duration = 100
+    if (duration == null || duration < 300) {
+        duration = 300
     }
     
     [
